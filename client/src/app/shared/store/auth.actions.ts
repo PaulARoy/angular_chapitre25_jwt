25197cad8ebb4deadc2e7d3a59db75a8713c0a99
Paul Roy <paul.roy@astriis.com>
2023-09-14 16:24:42 +0200

    210. actions

---
+++ client/src/app/shared/store/auth.actions.ts
@@ -0,0 +1,35 @@
+import { createAction, props } from '@ngrx/store';
+import { User } from '../interfaces/user.interface';
+
+export const tryFetchCurrentUserAction = createAction(
+  '[auth] try fetch current user'
+);
+export const fetchCurrentUserSuccessAction = createAction(
+  '[auth] fetch current user success',
+  props<{ user: User | null }>()
+);
+
+export const tryInscriptionAction = createAction(
+  '[auth] try inscription',
+  props<{ user: User }>()
+);
+export const inscriptionErrorAction = createAction(
+  '[auth] inscription error',
+  props<{ error: string }>()
+);
+
+export const tryConnexionAction = createAction(
+  '[auth] try connexion',
+  props<{ user: User }>()
+);
+export const connexionSuccessAction = createAction(
+  '[auth] connexion success',
+  props<{ user: User }>()
+);
+export const connexionErrorAction = createAction(
+  '[auth] connexion error',
+  props<{ error: string }>()
+);
+
+export const tryLogoutAction = createAction('[auth] try logout');
+export const LogoutSuccessAction = createAction('[auth] logout success');
--- client/src/app/shared/store/auth.reducer.ts
+++ client/src/app/shared/store/auth.reducer.ts
@@ -4,6 +4,7 @@ import { User } from '../interfaces/user.interface';
 export interface AuthState {
   user: User | null;
   isLoggedIn: boolean | null;
+  error: string | null;
 }
 
 export const authStateKey = '[auth] key';
@@ -11,6 +12,7 @@ export const authStateKey = '[auth] key';
 export const AUTH_INITIAL_STATE: AuthState = {
   user: null,
   isLoggedIn: null,
+  error: null,
 };
 
 export const authReducer = createReducer(AUTH_INITIAL_STATE);
