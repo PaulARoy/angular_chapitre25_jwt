ad4c98d50e9f6c0317eb8cb7fb2cf1353ab5c09b
Paul Roy <paul.roy@astriis.com>
2023-09-15 17:18:06 +0200

    218. adding unsplash

--- client/src/app/features/photos/shared/services/unsplash.service.ts
+++ client/src/app/features/photos/shared/services/unsplash.service.ts
@@ -1,16 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Photo } from '../interfaces/photo.interface';
+import { createApi } from 'unsplash-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UnsplashService {
+  private accesKey = 'd95NSjcpe--PsvG1TboDL2GBA8RIwLEOKNDC-J5ZcNs';
+
+  private unsplash = createApi({
+    accessKey: this.accesKey,
+  });
+
   constructor(private http: HttpClient) {}
 
   public searchPhotos(query: string): Observable<Photo[]> {
-    console.log(query);
-    return of([]);
+    console.log('searching', query);
+    return from(
+      this.unsplash.search.getPhotos({ query: query, orientation: 'squarish' })
+    ).pipe(
+      map((result: any) =>
+        result.response?.results.map((r: any) => ({ url: r.urls.small }))
+      )
+    );
   }
 }
